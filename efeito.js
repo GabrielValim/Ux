86f5a0f1a292e0102394e7dc208e14a066bcc5d7
GabrielValim <barros.gabriel.valim@hotmail.com>
2024-05-13 12:01:34 -0300

    first commit

---
+++ efeito.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".start")
+
+btn.onmousemove = function(e) {
+    var x = e.pageX - btn.offsetLeft;
+    var y = e.pageY - btn.offsetTop;
+
+    btn.style.setProperty("--eixoX", x + 'px')
+    btn.style.setProperty("--eixoY", y + 'px')
+}
